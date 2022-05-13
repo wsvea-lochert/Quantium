f2e1e0c0b35c8c9f615fdb8559e7aa6ebf7a6e04
William Svea-Lochert <dmuwilliamsl@gmail.com>
2022-05-14 00:39:19 +0200

    update

--- Lupin/LupinRunner.py
+++ Lupin/LupinRunner.py
@@ -3,7 +3,7 @@ from Lupin.LupinExamin import LupinExamin
 mobilenetExamin = LupinExamin(json='C:/Users/William/Documents/C3P0 datasets/greenscreen/testset/test.json', 
                               img_dir='C:/Users/William/Documents/C3P0 datasets/greenscreen/224x224_v1/',
                               kp_def='C:/Users/William/Documents/C3P0 datasets/greenscreen/dataset_head.csv', 
-                              model_dir='C:/Users/William/Documents/git/Quantium/Lupin/mobilenet/McFly_mobilenet_lr')
+                              model_dir='C:/Users/William/Documents/git/Quantium/Dobby/checkpoints/xMcFly_mobilenet')
 mobilenetExamin.run_test()
 # resnetExamin = LupinExamin()
 # cnnExamin = LupinExamin()
